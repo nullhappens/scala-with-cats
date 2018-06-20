c97ed6e9ddd575384ecb19ab359d52f6a58e7276
Diego Pineda <nullhappens@users.noreply.github.com>
2018-06-19 22:27:09 -0400

    added examples for eq and show

---
+++ src/main/scala/com/nullhappens/cats/eq/Main.scala
@@ -0,0 +1,30 @@
+package com.nullhappens.cats.eq
+
+import cats._
+import cats.implicits._
+import com.nullhappens.cats.printable.Cat
+
+object Main extends App {
+  implicit val catEq: Eq[Cat] = Eq.instance[Cat]{ (c1, c2) =>
+    c1.name === c2.name && c1.age === c2.age && c1.color === c2.color
+  }
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+  val cat3 = Cat("Garfield", 38, "orange and black")
+
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+  val optionCat3 = Option(cat3)
+
+  assert(cat1 === cat1)
+  assert(cat2 === cat2)
+  assert(cat1 === cat3)
+  assert(cat1 =!= cat2)
+
+  assert(optionCat1 === optionCat1)
+  assert(optionCat2 === optionCat2)
+  assert(optionCat1 === optionCat3)
+  assert(optionCat1 =!= optionCat2)
+
+}
---
+++ src/main/scala/com/nullhappens/cats/show/Main.scala
@@ -0,0 +1,10 @@
+package com.nullhappens.cats.show
+
+import cats._
+import cats.implicits._
+import com.nullhappens.cats.printable.Cat
+
+object Main extends App {
+  implicit val catShow: Show[Cat] = Show.show(cat => s"${cat.name.show} is a ${cat.age.show} year-old ${cat.color.show} cat.")
+  println(Cat("Mr Buttons", 5, "Brown").show)
+}
