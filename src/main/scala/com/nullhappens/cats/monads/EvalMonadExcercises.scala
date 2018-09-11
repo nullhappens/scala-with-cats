4bacc227a5edb141a3d574da25277b29939ea166
Diego Pineda <nullhappens@users.noreply.github.com>
2018-09-10 21:39:58 -0400

    added excercises for foldLeft

--- src/main/scala/com/nullhappens/cats/monads/EvalMonadExcercises.scala
+++ src/main/scala/com/nullhappens/cats/monads/EvalMonadExcercises.scala
@@ -85,4 +85,29 @@ object EvalMonadExcercises extends App {
 
   println(factorial(50000).value)
 
+  // Naive non stack safe foldRight
+//  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+//    as match {
+//      case head :: tail =>
+//        fn(head, foldRight(tail, acc)(fn))
+//      case Nil =>
+//        acc
+//    }
+
+  // Rewritten to be stack safe below:
+  def foldRightEval[A, B](as: List[A], acc: Eval[B])(fn: (A, Eval[B]) => Eval[B]): Eval[B] =
+    as match {
+      case head :: tail =>
+        Eval.defer(fn(head, foldRightEval(tail, acc)(fn)))
+      case Nil =>
+        acc
+    }
+
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+    foldRightEval(as, Eval.now(acc)){ (a, b) =>
+      b.map(fn(a, _))
+    }.value
+
+  println(foldRight((1 to 1000000).toList, 0L)(_ + _))
+
 }
